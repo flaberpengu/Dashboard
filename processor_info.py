621c2ef59942ac1a83f896a2f78fc4c8e3b99051
flaberpengu <jacob.swales@gmail.com>
2020-07-03 11:46:00 +0100

    Fixed processor_info.py so that it can be called from dashboard.py

--- processor_info.py
+++ processor_info.py
@@ -5,16 +5,16 @@ import numpy as np
 ##Colours used for bars on graph
 colours = ['red', 'blue', 'green']
 
-##Get the values to plot
-yVals = np.arange(1,9)
-xVals = psutil.cpu_percent(interval=5, percpu=True)
+def getCPUInfo():
+    ##Get the values to plot
+    yVals = np.arange(1,9)
+    xVals = psutil.cpu_percent(interval=5, percpu=True)
 
-##Plot a horizontal bar chart
-plt.barh(yVals, xVals, align='center', alpha=0.75, zorder=100, color=colours)
-plt.ylabel("CPU Cores")
-plt.xlabel("% Usage")
-plt.grid(zorder=0)
+    ##Plot a horizontal bar chart
+    plt.barh(yVals, xVals, align='center', alpha=0.75, zorder=100, color=colours)
+    plt.ylabel("CPU Cores")
+    plt.xlabel("% Usage")
+    plt.grid(zorder=0)
 
-##Save the graph
-plt.savefig('processor_info_percpu_usage.png', bbox_inches='tight', dpi=300)
-#plt.show()
+    ##Save the graph
+    plt.savefig('processor_info_percpu_usage.png', bbox_inches='tight', dpi=300)
