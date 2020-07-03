84b0f1d47bf05206fa3543fd6fdf7e02d36d5faf
flaberpengu <jacob.swales@gmail.com>
2020-07-04 00:58:48 +0100

    Updated text files. Changed backgrounds, added date and time to top bar.

--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,7 @@ import datetime as dt
 root = tk.Tk()
 root.geometry('650x1000')
 root.title('Dashboard')
+root.configure(bg='#bfbfbf')
 
 ##Displays top bar with date and time
 currentDateTime = dt.datetime.now()
@@ -22,16 +23,27 @@ else:
     else:
         dateBarString = "%i-%i-%i" % (currentDateTime.year, currentDateTime.month, currentDateTime.day)
 ##TODO - Add dynamic time
-datetimeBar = tk.Canvas(root, width=650, height=43, bg='#636363').place(x=-2, y=-2)
+timeBarString = "%i:%i" % (currentDateTime.hour, currentDateTime.minute)
+datetimeBar = tk.Canvas(root, width=650, height=43, bg='#636363', highlightthickness=0).place(x=0, y=0)
 lblDate = tk.Label(datetimeBar,
                    text=dateBarString,
                    bg='#636363',
                    fg='#ffffff',
-                   font=('Book Antiqua', 20)).place(x=6, y=3)
+                   font=('Book Antiqua', 20)).place(x=8, y=3)
+lblName = tk.Label(datetimeBar,
+                   text="Jacob's Dashboard",
+                   bg='#636363',
+                   fg='#ffffff',
+                   font=('Book Antiqua', 20)).place(x=212, y=3)
+lblTime = tk.Label(datetimeBar,
+                   text=timeBarString,
+                   bg='#636363',
+                   fg='#ffffff',
+                   font=('Book Antiqua', 20)).place(x=582, y=3)
 
 ##Gets and displays image of CPU use per core
 #processor_info.getCPUInfo()
 cpu_info_image = ImageTk.PhotoImage(Image.open('processor_info_percpu_usage.png'))
-#cpu_info = tk.Canvas(root, width = 
+cpu_info = tk.Canvas(root, width=650, height=300, bg='#636363', highlightthickness=0).place(x=0, y=45)
 
 tk.mainloop()
